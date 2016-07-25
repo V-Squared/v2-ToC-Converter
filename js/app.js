6ec4da096fc2727fc7aaf18614aa1bebe4702cf8
lukaschen <villagelukaschen@gmail.com>
2016-07-25 17:27:57 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,50 +1,63 @@
-var mylist = [];
+$('#convert').click(function() {
+  var mylist = [];
 
-var yml = "contents:";
+  var yml = "contents:";
 
-$("div.list > ul > li > a").each(function() {
+  var result = $.parseHTML($('textarea').val());
 
-  if ($(this).next().is("ul")) {
+  $(".list").empty();
 
-    var subitem = [];
-    var subitemYml = "\n    subitem:"
-    $($(this).next().children("li").children("a")).each(function() {
+  $(".list").append(result);
+
+  console.log(result);
+
+  $("div.list > ul > li > a").each(function() {
+
+    if ($(this).next().is("ul")) {
+
+      var subitem = [];
+      var subitemYml = "\n    subitem:"
+      $($(this).next().children("li").children("a")).each(function() {
+        var href = $(this).attr("href").replace("#",'');
+
+        subitemYml += "\n      - name: " + $(this).text() + "\n        url: " + href;
+
+        subitem.push({
+          "name": $(this).text(),
+          "url": href
+        })
+      });
+
       var href = $(this).attr("href").replace("#",'');
 
-      subitemYml += "\n      - name: " + $(this).text() + "\n        url: " + href;
+      yml += "\n  - name: " + $(this).text() + "\n    url: " + href + subitemYml;
 
-      subitem.push({
+      mylist.push({
+        "name": $(this).text(),
+        "url": href,
+        "subitem": subitem
+      });
+
+    } else {
+      var href = $(this).attr("href").replace("#",'');
+
+      yml += "\n  - name: " + $(this).text() + "\n    url: " + href;
+
+      console.log(yml);
+
+      mylist.push({
         "name": $(this).text(),
         "url": href
-      })
-    });
+      });
+    }
 
-    var href = $(this).attr("href").replace("#",'');
+  });
 
-    yml += "\n  - name: " + $(this).text() + "\n    url: " + href + subitemYml;
+  $("#resultYml").empty();
 
-    mylist.push({
-      "name": $(this).text(),
-      "url": href,
-      "subitem": subitem
-    });
-
-  } else {
-    var href = $(this).attr("href").replace("#",'');
-
-    yml += "\n  - name: " + $(this).text() + "\n    url: " + href;
-
-    console.log(yml);
-
-    mylist.push({
-      "name": $(this).text(),
-      "url": href
-    });
-  }
-
-});
-
-$("#resultYml").text(yml);
+  $("#resultYml").text(yml);
 
 // for result only
-$("#result").html(JSON.stringify(mylist))
+  $("#result").empty();
+  $("#result").html(JSON.stringify(mylist));
+});
